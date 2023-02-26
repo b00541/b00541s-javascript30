6633282d1d2feb2869240337ae3f163dff71a013
b00541 <ramanparmarphillips@googlemail.com>
2023-02-26 23:57:55 +0000

    26/02/2023

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,4 +1,6 @@
-//Get Elements
+//============
+//GET ELEMENTS
+//============
 const player      = document.querySelector('.player');
 const video       = player.querySelector('.viewer');
 const progress    = player.querySelector('.progress');
@@ -8,18 +10,13 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges      = player.querySelectorAll('.player__slider');
 const full        = player.querySelector('.fullScreen');
 
-//Functions
+//=========
+//FUNCTIONS
+//=========
 function togglePlay() {
   const method = video.paused ? 'play' : 'pause'
   video[method]()
-  // console.log(method)
-    // if (video.paused) {
-    //   video.play()
-    // } else {
-    //   video.pause()
-    // }
 }
-
 
 function updateButton() {
   const icon = this.paused ? '►' : '❚ ❚'
@@ -44,14 +41,46 @@ const scrubTime =(e.offsetX / progress.offsetWidth) *video.duration
 video.currentTime = scrubTime
 }
 
-function fullScreenVideo() {
-  //console.log(webkitDisplayingFullscreen:false);
-  console.log(video.webkitDisplayingFullscreen);
-  console.dir(video);
-  video.webkitDisplayingFullscreen = true
+function toggleFullScreen(e) {
+  if (isFullScreen) {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else {
+      console.error('Unable to find a fullscreen exit method.');
+    }
+    console.log('removing fullscreen class');
+  } else {
+    if (player.requestFullscreen) {
+      player.requestFullscreen(); // standard
+    } else if (player.webkitRequestFullscreen) {
+      player.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    } else if (player.mozRequestFullScreen) {
+      player.mozRequestFullScreen();
+    } else if (player.msRequestFullscreen) {
+      player.msRequestFullscreen();
+    } else {
+      console.error('Unable to find a fullscreen request method');
+    }
+  }
 }
-// video.controls = true
-fullScreenVideo()
+
+function toggleFullScreenClasses() {
+  player.classList.toggle('fullscreen');
+  isFullScreen = !isFullScreen;
+}
+
+let isFullScreen = false;
+
+//===============
+//EVENT LISTENERS
+//===============
+
 
 
 //Space bar for pause
@@ -60,18 +89,54 @@ window.addEventListener('keydown', (e) => {
     togglePlay()
   }
 })
+//forward and back skip on left and right button press
+window.addEventListener('keydown', (e) => {
+  // console.log(e.key);
+  if (e.key === 'ArrowRight') {
+    video.currentTime += 10
+  }
+})
+window.addEventListener('keydown', (e) => {
+  // console.log(e.key);
+  if (e.key === 'ArrowLeft') {
+    video.currentTime -= 10
+  }
+})
+window.addEventListener('keydown', (e) => {
+  // console.log(e.key);
+  if (e.key === 'ArrowUp') {
+    video.volume += 0.5
+  }
+})
+window.addEventListener('keydown', (e) => {
+  // console.log(e.key);
+  if (e.key === 'ArrowDown') {
+    video.volume -= 0.5
+  }
+})
 
+
+
+// Press f to pay respect
+window.addEventListener('keydown', (e) => {
+  // console.log(e.key);
+  if (e.key === 'f') {
+    toggleFullScreen()
+  }
+})
+
+full.addEventListener('click', toggleFullScreen);
 
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
 video.addEventListener('timeupdate', handleProgress)
 
+let mousedown = false
 progress.addEventListener('click', scrub)
 progress.addEventListener('mousemove', (e) =>  mousedown && scrub(e))
 progress.addEventListener('mousedown', () =>  mousedown = true)
 progress.addEventListener('mouseup',   () =>  mousedown = false)
-let mousedown = false
 
 toggle.addEventListener('click', togglePlay)
 
@@ -79,7 +144,11 @@ ranges.forEach(range => {
 range.addEventListener('change', handleRangeUpdate)
 })
 
-
 skipButtons.forEach(item => {
 item.addEventListener('click', skip);
 });
+
+document.addEventListener('fullscreenchange', toggleFullScreenClasses);
+document.addEventListener('mozfullscreenchange', toggleFullScreenClasses);
+document.addEventListener('webkitfullscreenchange', toggleFullScreenClasses);
+document.addEventListener('msfullscreenchange', toggleFullScreenClasses);
